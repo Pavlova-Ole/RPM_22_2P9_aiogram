21257a0fc5469b9402f0462450633b40caad30b8
TerminalNavi <156845332+Pavlova-Ole@users.noreply.github.com>
2024-04-19 08:58:30 +0300

    создание кнопок

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import asyncio 
  
-from aiogram import Bot, Dispatcher, Router 
-from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton 
-from aiogram.filters import Command  
+from aiogram import Bot, Dispatcher, Router , F
+from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardButton, InlineKeyboardMarkup,  CallbackQuery
+from aiogram.filters import Command 
 from aiogram.types.bot_command import BotCommand 
  
 bot = Bot(token="7173826357:AAFWJEthXIVu94kLaZwrQjECv2_mOjB8MPQ") 
@@ -16,13 +16,40 @@ async def start_handler(msg: Message):
         BotCommand(command='start', description='Запуск бота'), 
         BotCommand(command='set_time', description='Задать время рассылки'), 
         BotCommand(command='help', description='Справка') 
-    ]) 
-    await msg.answer(text="Привет") 
-     
-    markup = ReplyKeyboardMarkup(keyboard=[[KeyboardButton('привет')]]) 
+    ])
+    markup = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text='привет')]]) 
     await msg.answer(text="Привет", reply_markup=markup) 
+
+
+    inline_markup = InlineKeyboardMarkup(inline_keyboard = [
+        [
+            InlineKeyboardButton(text='Карина', callback_data='1'),
+            InlineKeyboardButton(text='Юля', callback_data='2')],
+        [
+            InlineKeyboardButton(text='Олеся', callback_data='3')]
+    ])
+    await msg.answer(text="Привет", reply_markup=inline_markup) 
  
- 
+
+
+@router.callback_query(F.data == "1")
+async def callback_query_handler(callback_query: CallbackQuery):
+    await callback_query.message.answer(text="разработчик")
+
+
+@router.callback_query(F.data == "2")
+async def callback_query_handler(callback_query: CallbackQuery):
+    await callback_query.message.answer(text="тестировщик")
+
+
+@router.callback_query(F.data == "3")
+async def callback_query_handler(callback_query: CallbackQuery):
+    await callback_query.message.answer(text="тимлид")
+
+@router.callback_query()
+async def callback_query_handler(callback_query: CallbackQuery):
+    await callback_query.message.answer(text=callback_query.data)
+
 @router.message(Command("help")) 
 async def start_handler(msg: Message): 
     await msg.answer(text="Крч ребята если бот лег то звонить павлОвой (+79109245056)") 
